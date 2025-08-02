766892d38d28027489585cb7ea44740548e3777a
ahmadtara <ahmadtara.pratama@gmail.com>
2025-08-03 01:31:02 +0700

    Update dwg.py

--- dwg.py
+++ dwg.py
@@ -187,10 +187,11 @@ def draw_to_template(classified, template_path):
                 text_layer = "FEATURE_LABEL"
                 text_color = 6
                 attribs = {
-                    "height": getattr(matchprop_hp, "height", 1.5) if matchprop_hp else 1.5,
+                    "height": 2.5,
                     "layer": text_layer,
                     "color": text_color,
-                    "insert": (x + 2, y)
+                    "insert": (x - 4, y - 1.8),
+                    "rotation": 0
                 }
                 msp.add_text(obj["name"], dxfattribs=attribs)
                 continue
