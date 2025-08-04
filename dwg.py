558f78b340eeb63f7328f4304e9c3f5e89a1fe15
ahmadtara <ahmadtara.pratama@gmail.com>
2025-08-04 22:31:22 +0700

    Update dwg.py

--- dwg.py
+++ dwg.py
@@ -5,12 +5,10 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import tempfile
 
-# SETUP GOOGLE SHEET
 SPREADSHEET_ID = "1yXBIuX2LjUWxbpnNqf6A9YimtG7d77V_AHLidhWKIS8"
 SHEET_NAME = "Pole Pekanbaru"
 
 def authenticate_google():
-    # Kalau pakai .streamlit/secrets.toml
     creds_dict = st.secrets["gcp_service_account"]
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
@@ -20,37 +18,39 @@ def authenticate_google():
 
 def extract_poles_from_kmz(kmz_path):
     poles = []
+
     with zipfile.ZipFile(kmz_path, 'r') as zf:
         with zf.open('doc.kml', 'r') as file:
             doc = file.read()
 
     k = kml.KML()
     k.from_string(doc)
-    
-    for d in k.features():
-        for folder in d.features():
-            if folder.name in ['NEW POLE 7-3', 'NEW POLE 7-4']:
-                for placemark in folder.features():
-                    name = placemark.name
-                    coord = list(placemark.geometry.coords)[0]  # (lon, lat)
-                    poles.append({
-                        'Pole_Id': name,
-                        'PoleName': name,
-                        'Latitude': coord[1],
-                        'Longitude': coord[0]
-                    })
+
+    for document in k.features():  # Level 1: <Document>
+        for outer_folder in document.features():  # Level 2: LINE A, LINE B, LINE C
+            for inner_folder in outer_folder.features():  # Level 3: NEW POLE 7-3/7-4
+                if inner_folder.name in ['NEW POLE 7-3', 'NEW POLE 7-4']:
+                    for placemark in inner_folder.features():
+                        if placemark.geometry:
+                            name = placemark.name
+                            coord = list(placemark.geometry.coords)[0]
+                            poles.append({
+                                'Pole_Id': name,
+                                'PoleName': name,
+                                'Latitude': coord[1],
+                                'Longitude': coord[0]
+                            })
     return poles
 
 def append_to_sheet(sheet, data):
     for item in data:
         row = [
-            "", "", "", "", "",                  # Subregion, ProvinceName, City, District, SubDistrict
+            "", "", "", "", "", 
             item['Pole_Id'], item['PoleName'],
             item['Latitude'], item['Longitude']
-        ] + [""] * 19  # Sisanya biar sesuai kolom
+        ] + [""] * 19
         sheet.append_row(row)
 
-# UI
 st.title("Uploader Pole KMZ ke Google Sheet")
 
 uploaded_file = st.file_uploader("Upload file .kmz", type=["kmz"])
@@ -62,7 +62,7 @@ if uploaded_file is not None:
 
     with st.spinner("Membaca dan memproses KMZ..."):
         poles = extract_poles_from_kmz(kmz_path)
-    
+
     if poles:
         st.success(f"{len(poles)} titik ditemukan. Mengirim ke Google Sheets...")
         try:
